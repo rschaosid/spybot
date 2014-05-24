0221f1ab07da9ae0161876376f8a4e6bbc931a9e
rschaosid <rschaosid@gmail.com>
2014-05-24 18:19:48 -0500

    tile backgrounds

--- public/scripts/spybot-human.js
+++ public/scripts/spybot-human.js
@@ -3,7 +3,7 @@ function SpybotHumanInterface(domElement) {
   //canvas.setAttribute('width', 650);
   //canvas.setAttribute('height', 420);
   //el.appendChild(canvas);
-  
+
 
   var bkg = document.createElement('div');
   bkg.classList.add('spybot-databattle-backdrop');
@@ -15,9 +15,10 @@ function SpybotHumanInterface(domElement) {
   for(var i=0;i<13;i++) {
     var row = document.createElement('tr');
     for(var j=0;j<16;j++) {
-      var square = document.createElement('td');
-      square.classList.add('spybot-databattle-board-square-passable');
-      row.appendChild(square);
+      var cell = document.createElement('td');
+      cell.classList.add('spybot-databattle-board-cell-passable');
+      cell.classList.add('spybot-databattle-board-cell-type-'+(1+Math.floor(Math.random()*9)));
+      row.appendChild(cell);
     }
     board.appendChild(row);
   }
